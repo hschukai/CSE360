b0d857905717c8ccd16109c98b3723b84f96a003
hallieschukai1 <hallieschukai1@60.dhcp.apogeetelecom.com>
2020-10-03 14:52:47 -0700

    Filled in code and added Javadoc comments.

--- AddingMachine.java/src/cse360assignment02/AddingMachine.java
+++ AddingMachine.java/src/cse360assignment02/AddingMachine.java
@@ -1,3 +1,10 @@
+/**
+ * Assignment 1
+ * Hallie Schukai
+ * CSE 360
+ * Due 5 Oct. 2020
+ */
+
 package cse360assignment02;
 
 public class AddingMachine {
@@ -11,12 +18,23 @@ public class AddingMachine {
     return 0;
   }
   
+  /**
+   * Add method takes a value and adds it from the total variable value.
+   * @param value - value to add
+   */
   public void add (int value) {
   }
 
+  /**
+   * Subtract method takes a value and subtracts it from the total variable value.
+   * @param value - value to subtract
+   */
   public void subtract (int value) {
   }
 
+  /**
+   * toString method; returns as a string.
+   */
   public String toString () {
     return "";
   }
